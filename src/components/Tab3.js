fa247984a317e434ea4f8fd37a7d239b9c462fa3
MainaJoseph <mainajm254@gmail.com>
2022-10-04 17:37:25 +0300

    new commit

---
+++ src/components/Tab3.js
@@ -0,0 +1,34 @@
+var Tab3 = React.createClass({
+    render: function() {
+      return (
+  
+        <div className="container">
+          <div className="card">
+            <div className="imgBx">
+              <img src="https://assets.codepen.io/4164355/shoes.png" />
+            </div>
+            <div className="contentBx">
+              <h2>Nike Shoes</h2>
+              <div className="size">
+                <h3>Size :</h3>
+                <span>7</span>
+                <span>8</span>
+                <span>9</span>
+                <span>10</span>
+              </div>
+              <div className="color">
+                <h3>Color :</h3>
+                <span />
+                <span />
+                <span />
+              </div>
+              <a href="#">Buy Now</a>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }); 
+  
+
+  export default Tab3;
